02caeba2dabb4c9a1c69ba6992f52866102db9d2
jularis <juliuskouame@gmail.com>
2025-07-28 09:30:36 +0000

    update

---
+++ resources/views/actualites/index.blade.php
@@ -0,0 +1,28 @@
+
+@extends('layouts.app')
+@section('title',$pageTitle)
+@section('description',$metaDescription)
+
+@section('content')
+    <!-- Start Breadcrumb
+    ============================================= -->
+    <div class="breadcrumb-area text-center bg-gray-gradient-secondary">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2">
+                    <h1>{{ $pageTitle }} </h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li><a href="#"><i class="fas fa-home"></i> Home</a></li>
+                            <li class="active">{{ $pageTitle }}</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Breadcrumb -->
+
+
+
+@endsection
