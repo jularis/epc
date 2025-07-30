9248e7cdb29e579ae41881953b911d645da92252
jularis <juliuskouame@gmail.com>
2025-07-30 08:37:49 +0000

    update

--- app/Models/Distinction.php
+++ app/Models/Distinction.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use TCG\Voyager\Traits\Translatable;
 
 class Distinction extends Model
 {
-    
+    use Translatable;
+    protected $translatable = ['title','description'];
 }
