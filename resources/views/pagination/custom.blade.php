5d61c6260e3a5ab1726b43f7b4efae10731021a2
jularis <juliuskouame@gmail.com>
2025-08-14 11:14:29 +0000

    update

--- resources/views/pagination/custom.blade.php
+++ resources/views/pagination/custom.blade.php
@@ -19,7 +19,7 @@
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                    <li class="page-item active"><a class="page-link" href="#">{{ $element }}</a></li>
+                    <li class="page-item active"><a class="page-link" href="#">{{ $page }}</a></li>
                     @else
                     <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
                     @endif
