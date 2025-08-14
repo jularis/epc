c73b1ff0b8d0889b68036f15e254a654fe0ac597
jularis <juliuskouame@gmail.com>
2025-08-14 14:08:44 +0000

    update

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProduitController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ActualiteController;
 use App\Http\Controllers\PartenaireController;
 
@@ -49,8 +50,19 @@ Route::group([
     Route::get('/nos-produits/{id}/{slug}', [ProduitController::class,'show']);
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::group(['prefix' => 'console-admin'], function () {
     Voyager::routes();
 
 });
+
+Auth::routes();
